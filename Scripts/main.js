d30b310fa8d3bb59aeae74f037c284b7deb1746c
onik-hoque <onik-hoque@codermerlin.codermerlin.com>
2023-12-19 17:35:55 -0600

    experimenting with cookies

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,21 +1,159 @@
+
+
+var soundEffects = document.querySelectorAll(".sfx");
+var music = document.getElementById("music");
+var overlay = document.getElementById('overlay');
+var overlayButtonContainers = document.getElementsByClassName("overlayButtonContainer");
+
+var switchSFX = document.getElementById("switch");
+
+var buttonsInteractable = false;
+
+var buttonYes = document.getElementById("soundButtonYes");
+var buttonNo = document.getElementById("soundButtonNo");
+
+Cookies.set("testCookie", "string!!");
+const optionsSelected = Cookies.get('testCookie') !== undefined;
+
+if (optionsSelected) {
+    console.log("cookie works :)");
+} else {
+    console.log("cookie doesn't work :(");
+}
+
+var musicQueued = false;
+
+var CRT = false;
+
+
+for (var i = 0; i < soundEffects.length; i++) {
+	    soundEffects[i].volume = 0;
+}
+
+
+//overlay.parentNode.removeChild(overlay);
+
+function overlayButtonAction(button) {
+
+    if (buttonsInteractable) {
+
+        for (var i = 0; i < overlayButtonContainers.length; i++) {
+            overlayButtonContainers[i].style.opacity = 0;
+        }
+        buttonsInteractable = false;
+
+	if (button.id == "CRTButtonYes") {
+	    typed.destroy();
+	    overlay.remove();
+
+	    if (!CRT) {
+		toggleCRT();
+	    }
+
+	    if (musicQueued) {
+		console.log("testing music")
+		music.play();
+	    }
+	}
+
+	if (button.id == "CRTButtonNo") {
+	    typed.destroy();
+	    overlay.remove();
+
+	    if (CRT) {
+		toggleCRT();
+	    }
+	}
+
+
+	if (button.id == "musicButtonYes") {
+	    typed.reset();
+	    typed.strings = ["Would you like to enable a CRT filter?"];
+	    musicQueued = true;
+	    buttonYes.id = "CRTButtonYes";
+	    buttonNo.id = "CRTButtonNo";
+	}
+
+	if (button.id == "musicButtonNo") {
+	    typed.reset();
+	    typed.strings = ["Would you like to enable a CRT filter?"];
+	    musicQueued = false;
+	    buttonYes.id = "CRTButtonYes";
+	    buttonNo.id = "CRTButtonNo";
+
+	}
+
+        if (button.id == "soundButtonYes") {
+	    
+            typed.reset();
+            typed.strings = ["Would you like to enable music?"];
+            //typed.start()
+            for (var i = 0; i < soundEffects.length; i++) {
+                soundEffects[i].volume = 1;
+            }
+
+	    buttonYes.id = "musicButtonYes";
+	    buttonNo.id = "musicButtonNo";
+	    
+        }
+
+	if (button.id == "soundButtonNo") {
+	    typed.reset();
+	    typed.strings = ["Would you like to enable music?"];
+	    buttonYes.id = "musicButtonYes";
+	    buttonNo.id = "musicButtonNo";
+	    for (var i = 0; i < soundEffects.length; i++) {
+                soundEffects[i].volume = 0;
+            }
+	}
+
+
+
+        switchSFX.play();
+        switchSFX.currentTime = 0
+
+    }
+
+}
+
+
+ 
+
+
+
+var typed = new Typed('#overlayText', {
+    strings: ["Welcome to Onik Hoque\'s Digital Portfolio.", "There are various highly recommended optional effects and features.", "Would you like to enable sound effects?"],
+    typeSpeed: 30,
+    showCursor: false,
+    onComplete: function() {
+	setTimeout(function() {
+ 	for (var i = 0; i < overlayButtonContainers.length; i++) {
+	    overlayButtonContainers[i].style.opacity = 1;
+	}
+	    buttonsInteractable = true;
+	}, 150);
+    }
+});
+
+
 //MAKE THIS WORK WITH RELATIVE LINK
 
 
-var CRT = true;
+
 
 function toggleCRT() {
     
     if (CRT == true) {
 	document.getElementById("rainbow").style.display = "none";
 	document.getElementById("flicker").style.display = "none";
-	document.getElementById("CRTImage").src= "../Images/crtdisabled.png";
+	document.getElementById("CRTImage").src= "Images/crtdisabled.png";
 	var audio = document.getElementById("switch");
 	audio.play();
 	CRT = false;
     } else if (CRT == false) {
 	document.getElementById("rainbow").style.display = "block";
 	document.getElementById("flicker").style.display = "block";
-	document.getElementById("CRTImage").src = "../Images/crt.png";
+	document.getElementById("CRTImage").src = "Images/crt.png";
 	var audio = document.getElementById("switch");
 	audio.play();
 	CRT = true;
@@ -51,9 +189,8 @@ document.querySelectorAll(".dropdown").forEach( item => {
 
 document.querySelectorAll(".grayLink").forEach( item => {
     item.addEventListener("mouseover", function( event ) {
-	var audio = document.getElementById("switch");
-	    audio.play();
-	    audio.currentTime = 0;
+	    switchSFX.play();
+	    switchSFX.currentTime = 0;
 	
     }, false);
 })
